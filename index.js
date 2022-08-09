7b6e5a08d5457e8aca09484773100990eabd6945
NaveeNsaNkar07 <naveensankar871@gmail.com>
2022-08-09 14:25:41 +0530

    solved refreshing issue

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ for DOM -> document
 
 const taskContainer = document.querySelector(".task__container");
 
+const globalStore = []; // some values -> localstorage objects
+
 //this is for readability 
 const generateNewCard = (dataObject) =>
-    `<div class="col-md-6 col-lg-4" id = ${dataObject.id}>
+  `<div class="col-md-6 col-lg-4" id = ${dataObject.id}>
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
     <button type="button" class="btn btn-outline-success">
@@ -42,24 +44,55 @@ const generateNewCard = (dataObject) =>
 </div>
 </div>`;
 
+const loadInitialCardData = () => {
+  //accessing localstorage to get tasky card data
+  const getCardData = localStorage.getItem("tasky"); //to get a new data i have to use getItem
+  //converting the data from string to normal object
+  const {cards} = JSON.parse(getCardData);
+  //looping over the array of task to create a HTML card, inject it to in DOM
+  cards.map((cardObject) => {
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+    // updating my global storage 
+    globalStore.push(cardObject);
+  })
 
-const saveChanges = () => {
-    const taskData = {
-        id: `${Date.now()}`,  //it will return unique number for id for every seconds 
-        imageUrl: document.getElementById("imageUrl").value,// value -> to access the value not the whole tag
-        taskTitle: document.getElementById("taskTitle").value,
-        taskType: document.getElementById("taskType").value,
-        taskDescription: document.getElementById("taskDescription").value,
-    };
 
-    console.log(taskData);
-
-    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
 };
 
-//close the modal after save -> oneline code
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`,  //it will return unique number for id for every seconds 
+    imageUrl: document.getElementById("imageUrl").value,// value -> to access the value not the whole tag
+    taskTitle: document.getElementById("taskTitle").value,
+    taskType: document.getElementById("taskType").value,
+    taskDescription: document.getElementById("taskDescription").value,
+  };
+
+  console.log(taskData);
+
+  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+
+  globalStore.push(taskData);
+
+  localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+};
+
+//close the modal after save -> oneline code -> data-bs-dismiss
 
 /* issues 
-    1) Modal is not closing after onclick -> after save changes
-    2) whenever refreshing i lost the data
+    1) Modal is not closing after onclick -> after save changes -> solved with data-bs-dismiss
+    2) whenever refreshing i lost the data -> to solve this i have to use -> LOCAL STORAGE
+
+    local storage -> It is API -> Application Programming interface
+    Application -> can be web app or mobile app or browser app -> But here Local Storage is our APP
+    Programming -> Accessing application via Programming
+    Interface -> Middle man -> accessing application programmatically
+
+    we can access the local storage via javascript with some methods 
+ */
+
+/* Features
+    1) Delete the card❌
+    2) Edit the card📝
+    3) Open the Card📖
  */
